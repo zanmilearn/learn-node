4db8df6276fee32823f541c25b15b2bd38001be4
Nagaraj <raj.nagaraj1990@gmail.com>
2016-12-18 12:59:06 +0530

    port change

--- index.js
+++ index.js
@@ -2,37 +2,44 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-   
+
     var sql = require("mssql");
 
     // config for your database
     var config = {
         user: 'Zanmi',
         password: 'zanmi@123',
-        server: '184.168.194.68', 
-        database: 'ZanmiLearn' 
+        server: '184.168.194.68',
+        database: 'ZanmiLearn'
     };
+
+    // set the port of our application
+    // process.env.PORT lets the port be set by Heroku
+    var port = process.env.PORT || 8080;
+
+    // set the view engine to ejs
+    app.set('view engine', 'ejs');
 
     // connect to your database
     sql.connect(config, function (err) {
-    
+
         if (err) console.log(err);
 
         // create Request object
         var request = new sql.Request();
-           
+
         // query to the database and get the records
         request.query('select * from Courses', function (err, recordset) {
-            
+
             if (err) console.log(err)
 
             // send records as a response
             res.send(recordset);
-            
+
         });
     });
 });
 
-var server = app.listen(5000, function () {
+app.listen(port, function () {
     console.log('Server is running..');
 });

845f6481eb9034ba72d401ea01b890098b077cbd
Nagaraj <raj.nagaraj1990@gmail.com>
2017-01-08 15:03:26 +0530

    revert

--- api/controllers/courses.js
+++ api/controllers/courses.js
@@ -20,33 +20,25 @@ exports.createCourse = function (req, res) {
     _course.topics = [];
 
     if ((!_coursebase.topics) || _coursebase.topics.length === 0) {
-        Course.findOneAndUpdate({ courseNodeId: _course.courseNodeId }, _course, { upsert: true, new: true }, function (err, course) {
-            if (err) {
-                return res.status(400).send({
-                    message: err
-                });
-            } else {
-                res.json(course);
-            }
-        });
+        _coursebase.topics = [];
+        _coursebase.topics.push({ "topicNodeId": "0" })
     }
 
     _coursebase.topics.forEach(function (_topic, idx, array) {
         Topic.findOne({ topicNodeId: _topic.topicNodeId }, function (err, _seltopic) {
-            if (_seltopic) {
+            if (_seltopic)
                 _course.topics.push(_seltopic._id);
 
-                if (idx === array.length - 1) {
-                    Course.findOneAndUpdate({ courseNodeId: _course.courseNodeId }, _course, { upsert: true, new: true }, function (err, course) {
-                        if (err) {
-                            return res.status(400).send({
-                                message: err
-                            });
-                        } else {
-                            res.json(course);
-                        }
-                    });
-                }
+            if (idx === array.length - 1) {
+                Course.findOneAndUpdate({ courseNodeId: _course.courseNodeId }, _course, { upsert: true, new: true }, function (err, course) {
+                    if (err) {
+                        return res.status(400).send({
+                            message: err
+                        });
+                    } else {
+                        res.json(course);
+                    }
+                });
             }
         });
     });
@@ -79,7 +71,7 @@ exports.deleteCourse = function (req, res) {
 
 exports.getAllCourses = function (req, res) {
 
-
+    
 
     Course.find().populate('topics').exec(function (err, courses) {
         if (err) {
